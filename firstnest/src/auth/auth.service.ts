79b13d035071344425bf3e11a441068cefb3a703
Umang1420 <umangchaudhari670@gmail.com>
2026-09-10 18:14:33 +0530

    Practice

--- firstnest/src/auth/auth.service.ts
+++ firstnest/src/auth/auth.service.ts
@@ -1,25 +1,3 @@
-// import { JwtService } from '@nestjs/jwt';
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { UsersService } from '../users/users.service.js';
-
-// @Injectable()
-// export class AuthService {
-//   constructor(private usersService: UsersService,
-//     private jwtService: JwtService) {}
-
-//   async signIn(username: string, pass: string): Promise< {access_token : string}> {
-//     const user = await this.usersService.findOne(username);
-//     if (user?.password !== pass) {
-//       throw new UnauthorizedException();
-//     }
-//     const payload = {sub : user.id , username : user.userEmail ,roles: [user.role]};
-
-//     return {
-//         access_token : await this.jwtService.signAsync(payload)
-//     };
-//   }
-// }
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service.js';
